28868e734c75dc5f4a37aa60f10a695269cb570b
Rohan Arepally <rarepall@uber.com>
2016-01-17 15:03:53 -0800

    config

--- app/server.js
+++ app/server.js
@@ -1,12 +1,23 @@
 'use strict';
 
 var express = require('express');
+<<<<<<< 85a9c429d40cc0b54709f1b4abf2abe928e8960a
 var app = express();
 
 app.use(express.static('public'));
 
 app.get('/', function (req, res) {
     res.send('Hello World');
+=======
+
+// Constants
+var PORT = process.env.PORT || 8080;
+
+// App
+var app = express();
+app.get('/', function (req, res) {
+    res.send('Hello Rohan\n');
+>>>>>>> heroku configs
 });
 
 

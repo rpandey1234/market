905b58116d866751e6ab915e05fa2f1a61ee84ef
Rahul Pandey <rahul@pinterest.com>
2016-01-17 15:02:18 -0800

    like heroku

--- server.js
+++ server.js
@@ -4,16 +4,17 @@ var express = require('express');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
-
 app.use(express.static(__dirname + '/public'));
+
+app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/', function (req, res) {
     res.render('pages/index');
 });
 
-
 var port = app.get('port');
 var server = app.listen(port, function () {
+    console.log(__dirname);
     console.log('Running on http://localhost:%s', port);
 });
